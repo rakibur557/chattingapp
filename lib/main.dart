52d52ccbc097b96031737cac3dd60bddcd7087f3
rakibur557 <rakiburrahman.cse.eng@gmail.com>
2021-10-27 20:01:04 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chattingapp/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedSplashScreen(
+        splash: Image.asset('images/chaticon.png'),
+        nextScreen: HomePage(),
+        splashTransition: SplashTransition.scaleTransition,
+      ),
+    );
+  }
+}
